dff6418b2592eb25dcb27dc9cb1c56978b1ce081
Polinaa19 <43252722+Polinaa19@users.noreply.github.com>
2018-10-01 19:29:54 +0300

    Add files via upload

--- sum.py
+++ sum.py
@@ -4,9 +4,7 @@ import numbers
 def my_sum(a, b):
     """
     Sums two variables and returns the result.
-
     Both parameters should be numbers.
-
     :param a: first addend
     :param b: second addend
     :return: sum of the addends
@@ -18,7 +16,6 @@ def my_sum(a, b):
 def is_number(x):
     """
     Check whether the object provided is a number.
-
     :param x: object to check
     :return: True if x is a number
     """
